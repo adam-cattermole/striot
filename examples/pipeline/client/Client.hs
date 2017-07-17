1fa5059ace221f54e90a8c9304d953b74b096edb
Adam Cattermole <a.cattermole@newcastle.ac.uk>
2017-07-17 12:18:17 +0100

    First attempt filtering by topic

--- examples/pipeline/client/Client.hs
+++ examples/pipeline/client/Client.hs
@@ -7,7 +7,11 @@ import Striot.FunctionalProcessing
 import Striot.FunctionalIoTtypes
 import Striot.Nodes
 import Network
+
 import qualified Network.MQTT as MQTT
+import Data.Text (Text)
+import Data.ByteString (ByteString)
+import Control.Monad(when)
 
 portNum  = 9002::PortNumber
 hostName = "haskellclient2"::HostName
@@ -47,7 +51,7 @@ main = do
 
     -- atomically (readTChan pubChan)
     threadDelay (1 * 1000 * 1000)
-    nodeSource (getMqttMsg pubChan) streamGraph2 hostName portNum -- processes source before sending it to another node
+    nodeSource (getMqttMsgByTopic pubChan accelT) streamGraph2 hostName portNum -- processes source before sending it to another node
 
 streamGraph2 :: Stream String -> Stream String
 streamGraph2 = streamMap Prelude.id
@@ -57,7 +61,26 @@ getMqttMsg pubChan = atomically (readTChan pubChan) >>= handleMsg
 
 handleMsg :: MQTT.Message 'MQTT.PUBLISH -> IO String
 handleMsg msg = do
+    let (t,p,l) = extractMsg msg
+    -- let t = MQTT.topic $ MQTT.body msg
+    --     p = MQTT.payload $ MQTT.body msg
+    --     l = MQTT.getLevels t
+    return $ read (show t) ++ " " ++ read (show p)
+
+getMqttMsgByTopic :: TChan (MQTT.Message 'MQTT.PUBLISH) -> MQTT.Topic -> IO String
+getMqttMsgByTopic pubChan topic = atomically (readTChan pubChan) >>= handleMsgByTopic topic
+
+handleMsgByTopic :: MQTT.Topic -> MQTT.Message 'MQTT.PUBLISH -> IO String
+handleMsgByTopic topic msg = do
+    let (t,p,l) = extractMsg msg
+    if topic == t then
+        return $ read (show t) ++ " " ++ read (show p)
+    else
+        return ""
+
+extractMsg :: MQTT.Message 'MQTT.PUBLISH -> (MQTT.Topic, ByteString, [Text])
+extractMsg msg =
     let t = MQTT.topic $ MQTT.body msg
         p = MQTT.payload $ MQTT.body msg
         l = MQTT.getLevels t
-    return $ read (show t) ++ " " ++ read (show p)
+    in (t,p,l)
