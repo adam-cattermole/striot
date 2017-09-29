1afbfd614a01eae264e6a0875948a0c61cf3e4d4
Adam Cattermole <a.cattermole@newcastle.ac.uk>
2017-09-29 12:06:44 +0100

    Moving WhiskRest from separate package

--- containers/build.sh
+++ containers/build.sh
@@ -8,11 +8,11 @@ cp -R ../license.txt striot-base/striot/
 cp -R ../striot.cabal striot-base/striot/
 cp -R ../src/* striot-base/striot/src
 
-echo "Copying whisk-rest"
-mkdir -p striot-base/whisk-rest/
-cp -R ../src/whisk-rest/LICENSE striot-base/whisk-rest/
-cp -R ../src/whisk-rest/whisk-rest.cabal striot-base/whisk-rest/
-cp -R ../src/whisk-rest/src striot-base/whisk-rest/
+# echo "Copying whisk-rest"
+# mkdir -p striot-base/whisk-rest/
+# cp -R ../src/whisk-rest/LICENSE striot-base/whisk-rest/
+# cp -R ../src/whisk-rest/whisk-rest.cabal striot-base/whisk-rest/
+# cp -R ../src/whisk-rest/src striot-base/whisk-rest/
 
 echo "Building"
 docker build -t $prefix/striot-base striot-base
