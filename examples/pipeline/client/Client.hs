6d4b521b5a6c09e5541502f7e10d902095859452
Adam Cattermole <a.cattermole@newcastle.ac.uk>
2017-10-13 11:28:13 +0100

    Initial attempt at collecting performance metrics

--- examples/pipeline/client/Client.hs
+++ examples/pipeline/client/Client.hs
@@ -1,4 +1,5 @@
 import Control.Concurrent
+import Control.Concurrent.STM
 import System.IO
 import Striot.FunctionalProcessing
 import Striot.FunctionalIoTtypes
@@ -11,15 +12,64 @@ hostName = "haskellclient2"::HostName
 main :: IO ()
 main = do
          threadDelay (1 * 1000 * 1000)
-         nodeSource src1 streamGraph2 hostName portNum -- processes source before sending it to another node
+         indexChan <- newTChanIO
+         let start = 1
+             rate  = 10000
+             swrate = 100
+         atomically $ writeTChan indexChan start
+         _ <- forkIO $ generator2 indexChan rate rates
+         nodeSource (src1 indexChan) streamGraph2 hostName portNum -- processes source before sending it to another node
 
-streamGraph2 :: Stream String -> Stream String
-streamGraph2 s = streamMap (\st-> st++st) s
+streamGraph2 :: Stream Int -> Stream Int
+streamGraph2 = Prelude.id
 
-src1:: IO String
-src1 = clockStreamNamed "Hello from Client!" 1000
+src1 :: TChan Int -> IO Int
+src1 indexChan = waitDelay indexChan
 
-clockStreamNamed:: String -> Int -> IO String -- returns the (next) payload to be added into an event and sent to a server
-clockStreamNamed message period = do -- period is in ms
-                                    threadDelay (period*1000)
-                                    return message
+rates :: [Int]
+rates = [1,5,10,20,50,100,200,500,1000,10000]
+
+swrates :: [Int]
+swrates =
+    let f   = 1       -- freq
+        fs  = 800     -- sample rate
+        hzmiddle = 250
+    in  cycle [ceiling((sin (2*pi*f*(x/fs))+1)*hzmiddle) |  x <- [0..fs]]
+
+message :: Int
+message = 1
+
+hz :: Int -> Int
+hz x
+    | x == 0 = f 1
+    | otherwise = f x
+    where f = round . (1000000 /) . fromIntegral
+
+waitDelay :: TChan Int -> IO Int
+waitDelay indexChan = do
+    i <- atomically (peekTChan indexChan)
+    threadDelay (hz i)
+    return i
+
+
+
+generator :: TChan Int -> Int -> Int -> IO b
+generator indexChan rate index = do
+    let newi = min (index+1) (subtract 1 $ length rates)
+    threadDelay (rate*1000000)
+    -- read off last after we write to update the current queue head
+    atomically $ writeTChan indexChan newi
+    atomically $ readTChan indexChan
+    generator indexChan rate newi
+
+generator2 :: TChan a -> Int -> [a] -> IO ()
+generator2 indexChan rate [] = print "Gen at max"
+generator2 indexChan rate (x:xs) = do
+    gen' indexChan rate x
+    generator2 indexChan rate xs
+
+gen' :: TChan a -> Int -> [a] -> IO ()
+gen' indexChan rate x = do
+    atomically $ writeTChan indexChan x
+    atomically $ tryReadTChan indexChan
+    threadDelay (rate*1000)
