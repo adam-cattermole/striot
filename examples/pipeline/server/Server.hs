d91a848f66190239a1f0687fad657cc9fe613f98
Adam Cattermole <a.cattermole@newcastle.ac.uk>
2017-10-24 16:19:09 +0100

    Minor changes and fixes. First collection of data

--- examples/pipeline/server/Server.hs
+++ examples/pipeline/server/Server.hs
@@ -17,50 +17,40 @@ listenPort = 9001 :: PortNumber
 main :: IO ()
 main = do
     writeFile "sw-log.txt" ""
-    nodeSink kaliParse printStreamDelay listenPort
+    nodeSink streamGraph1 printStreamDelay listenPort
 
-streamGraphid :: Stream String -> Stream String
+streamGraphid :: Stream [Int] -> Stream [Int]
 streamGraphid = Prelude.id
 
-kaliParse :: Stream String -> Stream UTCTime
-kaliParse = streamMap (\x -> posixSecondsToUTCTime . fromRational $ fst (head (kaliParse' x)) / 1000000)
+streamGraph1 :: Stream [Int] -> Stream [Int]
+streamGraph1= streamWindowAggregate (chopTime 1) fn
 
-kaliParse' x = readHex $ filter (/='.') (splitOn "-" x !! 1)
-
--- streamGraph1 :: Stream Int -> Stream [Int]
--- streamGraph1= streamWindowAggregate (chopTime 1) fn
-
--- fn :: [Int] -> [Int]
--- fn e@(x:xs) =
---     let l = length e
---         currenthz = averageVal l (head e)
---         client2hz = averageVal l (last e) -- averaging over window in case it changes mid window
---     in [currenthz, client2hz, l]
--- fn [] = [0]
+fn :: [[Int]] -> [Int]
+fn e@(x:xs) =
+    let l = length e
+        currenthz = averageVal l (map head e)
+        client2hz = averageVal l (map last e) -- averaging over window in case it changes mid window
+    in [currenthz, client2hz, l]
+fn [] = [0]
 
 averageVal :: Int -> [Int] -> Int
 averageVal l xs = sum xs `div` l
 
-printStreamDelay :: Stream UTCTime -> IO ()
+printStreamDelay :: Stream [Int] -> IO ()
 printStreamDelay (e@(E id t v):r) = do
     now <- getCurrentTime
-    let newe = mapTimeDelay delay e where delay = diffUTCTime now v
-    print newe
+    let newe = mapTimeDelay delay e where delay = diffUTCTime now t
     appendFile "sw-log.txt" (show newe ++ "\n")
+    print newe
     printStreamDelay r
 printStreamDelay (e:r) = do
     print e
     printStreamDelay r
 printStreamDelay [] = return ()
 
-printStream :: Stream Int -> IO ()
-printStream (e:r) = do
-    print e
-    printStream r
-
-mapTimeDelay :: NominalDiffTime -> Event UTCTime -> Event (UTCTime, Float)
+mapTimeDelay :: NominalDiffTime -> Event [Int] -> Event ([Int], Float)
 mapTimeDelay delay (E id t v) = E id t newv
-    where newv = (v, roundN 10 (toRational delay))
+    where newv = (v, roundN 3 (toRational delay))
 
 roundN :: Int -> Rational -> Float
 roundN n f = fromInteger (round $ f * (10^n)) / (10.0^^n)
