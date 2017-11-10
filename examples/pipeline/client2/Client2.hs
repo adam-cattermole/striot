4aa32f3b273141e6df6b7a32af73f92150c6bc21
Adam Cattermole <a.cattermole@newcastle.ac.uk>
2017-11-10 14:55:27 +0000

    New metric collection method

--- examples/pipeline/client2/Client2.hs
+++ examples/pipeline/client2/Client2.hs
@@ -10,10 +10,13 @@ connectPort = 9001 :: PortNumber
 connectHost = "haskellserver" :: HostName
 
 main :: IO ()
-main = nodeLink streamGraph1 listenPort connectHost connectPort
+main = nodeLink streamGraphid listenPort connectHost connectPort
 
-streamGraph1 :: Stream Int -> Stream [Int]
-streamGraph1 = streamWindowAggregate (slidingTime 1) fn
+streamGraphid :: Stream (Int, Int) -> Stream (Int, Int)
+streamGraphid = Prelude.id
 
-fn :: [Int] -> [Int]
-fn ys@(x:xs) = [x, length ys]
+-- streamGraph1 :: Stream Int -> Stream [Int]
+-- streamGraph1 = streamWindowAggregate (slidingTime 1) fn
+
+-- fn :: [Int] -> [Int]
+-- fn ys@(x:xs) = [x, length ys]
