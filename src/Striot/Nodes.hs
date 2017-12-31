c2d064191bc0cf2011108f5dd9becf93ff6a4922
Adam Cattermole <a.d.cattermole@gmail.com>
2017-12-31 16:23:53 +0000

    Minor changes

--- src/Striot/Nodes.hs
+++ src/Striot/Nodes.hs
@@ -101,6 +101,7 @@ nodeSource :: (ToJSON (Event beta), Show beta) => IO alpha -> (Stream alpha -> S
 nodeSource pay streamGraph host port = do
     putStrLn "Starting source ..."
     stream <- readListFromSource pay
+    putStrLn "Starting source ..."
     let result = streamGraph stream
     sendStream result host port -- or printStream if it's a completely self contained streamGraph
 
