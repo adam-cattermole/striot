c0835134140b5390cdf2414f9484177d7c97fa38
Adam Cattermole <a.d.cattermole@gmail.com>
2018-05-10 15:27:16 +0100

    Multithreading server code

--- examples/pipeline/server/Server.hs
+++ examples/pipeline/server/Server.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 import System.IO
 import System.IO.Unsafe (unsafeInterleaveIO)
 import Data.List
@@ -5,53 +6,85 @@ import Data.List
 import Striot.FunctionalProcessing
 import Striot.FunctionalIoTtypes
 import Striot.Nodes
-import Network
+import Network.Socket (ServiceName)
 
 import Data.Time (getCurrentTime, diffUTCTime, NominalDiffTime)
 import Data.Time.Clock (UTCTime)
 import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
 import Data.List.Split
+import qualified Data.ByteString.Lazy.Char8 as BLC (hPutStrLn)
 import Numeric
 import Data.Aeson
+import qualified Data.Text as T
+import qualified Data.Text.Read as TR
+import Data.Either.Combinators
+import Control.Monad (when)
+import Control.Concurrent
+import Control.Concurrent.STM
 
-listenPort = 9001 :: PortNumber
+listenPort = "9001" :: ServiceName
+fileName = "sw-log.txt"
 
 main :: IO ()
 main = do
-    writeFile "sw-log.txt" ""
-    nodeSink kaliParse printStreamDelay listenPort
+    chan <- newTChanIO
+    writeFile fileName ""
+    hdl <- openFile fileName WriteMode
+    hSetBuffering hdl NoBuffering
+    _ <- forkFinally (writeToFile hdl chan) (\_ -> hClose hdl)
+    nodeSink streamGraphid (sinkToChan chan) listenPort
     -- nodeSink streamGraphid printStream listenPort
 
-streamGraphid :: Stream String -> Stream String
-streamGraphid = Prelude.id
+streamGraphid :: Stream T.Text -> Stream T.Text
+streamGraphid = streamMap (\x -> Prelude.id x)
 
 kaliParse :: Stream String -> Stream UTCTime
 kaliParse = streamMap (\x -> posixSecondsToUTCTime . fromRational $ fst (head (kaliParse' x)) / 1000000)
 
 kaliParse' x = readHex $ take 16 (drop 13 x)
 
--- streamGraph1 :: Stream Int -> Stream [Int]
--- streamGraph1= streamWindowAggregate (chopTime 1) fn
+kaliParse'' :: Stream T.Text -> Stream UTCTime
+kaliParse'' = streamMap (\x -> posixSecondsToUTCTime . fromRational $ (kaliParse''' x) /1000000)
 
--- fn :: [Int] -> [Int]
--- fn e@(x:xs) =
---     let l = length e
---         currenthz = averageVal l (head e)
---         client2hz = averageVal l (last e) -- averaging over window in case it changes mid window
---     in [currenthz, client2hz, l]
--- fn [] = [0]
+kaliParse''' :: T.Text -> Rational
+kaliParse''' x =
+    let e = kaliParse'''' x
+        (i, t) = fromRight' e
+    in  fromIntegral i
+
+kaliParse'''' :: T.Text -> Either String (Integer, T.Text)
+kaliParse'''' x = TR.hexadecimal $ T.take 16 (T.drop 13 x)
 
 averageVal :: Int -> [Int] -> Int
 averageVal l xs = sum xs `div` l
 
-printStreamDelay :: (ToJSON alpha) => Stream alpha -> IO ()
+
+sinkToChan :: (ToJSON alpha) => TChan (Event (alpha, UTCTime)) -> Stream alpha -> IO ()
+sinkToChan chan stream = do
+    out <- mapDelay stream
+    mapM_ (writeEvent chan) out
+  where
+      writeEvent chan x = atomically $ writeTChan chan x
+
+writeToFile :: (ToJSON alpha) => Handle -> TChan (Event (alpha, UTCTime)) -> IO ()
+writeToFile hdl chan = do
+    stream <- readEventsTChan chan
+    hPutLines'' hdl stream
+
+readEventsTChan :: ToJSON alpha => TChan (Event (alpha, UTCTime)) -> IO (Stream (alpha, UTCTime))
+readEventsTChan eventChan = System.IO.Unsafe.unsafeInterleaveIO $ do
+    x <- atomically $ readTChan eventChan
+    xs <- readEventsTChan eventChan
+    return (x : xs)
+
+
+printStreamDelay :: (ToJSON alpha, Show alpha) => Stream alpha -> IO ()
 printStreamDelay stream = do
-    hdl <- openFile "sw-log.txt" WriteMode
+    hdl <- openFile fileName WriteMode
     hSetBuffering hdl NoBuffering
     output <- mapDelay stream
-    hPutLines' hdl output
-    hClose hdl
-    printStreamDelay stream
+    hPutLines'' hdl output
+
 
 mapDelay :: Stream alpha -> IO (Stream (alpha, UTCTime))
 mapDelay (e@(E id t v):r) = unsafeInterleaveIO $ do
@@ -63,6 +96,14 @@ mapDelay (e@(E id t v):r) = unsafeInterleaveIO $ do
         now <- getCurrentTime
         return x { value = (v, now) }
 
+
+hPutLines'' :: ToJSON (Event alpha) => Handle -> Stream alpha -> IO ()
+hPutLines'' handle (x:xs) = do
+    writable <- hIsWritable handle
+    when writable $ do
+        BLC.hPutStrLn handle (encode x)
+        hFlush handle
+        hPutLines'' handle xs
 
 -- printStreamDelay :: Stream UTCTime -> IO ()
 -- printStreamDelay (e@(E id t v):r) = do
