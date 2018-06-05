e9a9a969be1dc6d887d82cb870bcbfb5c59677e0
Adam Cattermole <a.d.cattermole@gmail.com>
2018-06-05 17:08:42 +0100

    Changes to collect performance results

--- examples/k8s-pipeline/run.sh
+++ examples/k8s-pipeline/run.sh
@@ -3,16 +3,21 @@ NORMAL="\\033[0;39m"
 RED="\\033[1;31m"
 BLUE="\\033[1;34m"
 
-PREFIX="striot"
+PREFIX="adamcattermole"
 SINK="server"
+
+RESULTS_DIR="output"
+
+POD_COUNT="kubectl get pods --field-selector=status.phase=Running -o name | wc -l"
+
+TEST_TIME=1500
 
 set -e
 
-declare -a dirs=("server" "client2" "client")
+declare -a dirs=("server" "client2" "gen-broker" "generator")
 
 build() {
-  eval $(minikube docker-env)
-  log "Creating docker containers..."
+    log "Creating docker containers..."
   if [ -n "$1" ]; then
     if  [ $1 = "base" ]; then
       log "Making striot/striot-base..."
@@ -26,50 +31,81 @@ build() {
     names+=($n)
     log $PREFIX/$n
     docker build -t $PREFIX/$n $dir/
+    docker push $PREFIX/$n:latest
   done
-  log "Creating Kubernetes images..."
-  for name in "${names[@]:1}"
+
+  log "Creating Kubernetes services..."
+  for name in "${names[@]}"
   do
     log $name
-    kubectl run $name --image=$PREFIX/$name:latest --port=9001 --replicas=0 --image-pull-policy='IfNotPresent' --expose -l name=$name,all-flag=striot
+    # kubectl run $name --image=$PREFIX/$name:latest --port=9001 --replicas=0 --image-pull-policy='IfNotPresent' --expose -l name=$name,all-flag=striot
+    kubectl run $name --image=$PREFIX/$name:latest --port=9001 --replicas=0 --image-pull-policy='Always' --expose -l name=$name,all-flag=striot
   done
+
   log "Creating AcitveMQ Broker"
-  kubectl run amq-broker --image=webcenter/activemq:latest --replicas=0 --image-pull-policy='IfNotPresent' -l name=amq-broker,all-flag=striot
-  kubectl expose deployment amq-broker --port=8161 --target-port=8161 --name=amq-web --type='NodePort' -l name=amq-broker,all-flag=striot
+  kubectl run amq-broker --image=webcenter/activemq:latest --replicas=0 --image-pull-policy='IfNotPresent' -l name=amq-broker,all-flag=striot --env="ACTIVEMQ_ADMIN_LOGIN=admin" --env="ACTIVEMQ_ADMIN_PASSWORD=new_password"
+  kubectl expose deployment amq-broker --port=8161 --target-port=8161 --name=amq-web --type='LoadBalancer' -l name=amq-broker,all-flag=striot
   kubectl expose deployment amq-broker --port=61616 --target-port=61616 --name=amq-default -l name=amq-broker,all-flag=striot
   kubectl expose deployment amq-broker --port=61613 --target-port=61613 --name=amq-stomp -l name=amq-broker,all-flag=striot
 }
 
+
 start() {
   log "Starting pipeline..."
   kubectl scale deployment amq-broker --replicas=1
-  minikube service amq-web --url
-  kubectl run haskell-server -it --image=$PREFIX/haskell-server:latest --port=9001 --image-pull-policy='IfNotPresent' --attach=False --expose -l name=haskell-server,all-flag=striot
+  # minikube service amq-web --url
+  # kubectl run haskell-server -it --image=$PREFIX/haskell-server:latest --port=9001 --image-pull-policy='Always' --attach=False --expose -l name=haskell-server,all-flag=striot
+  pod_count=1
   declare -a names=()
-  for dir in "${dirs[@]:1}"
+  for dir in "${dirs[@]}"
   do
     n=haskell-$dir
     names+=($n)
     log $n
     kubectl scale deployment $n --replicas=1
+    ((pod_count++))
+    while [[ $(eval $POD_COUNT) -ne $pod_count ]]; do
+      log "Pods not running yet..."
+      sleep 5
+    done
   done
 }
 
-attach() {
+
+output() {
+  # kubectl logs -f --pod-running-timeout=1h --timestamps=true deploy/haskell-server
   kubectl attach -it deploy haskell-server -c haskell-server
+}
+
+
+extract() {
+  log "Extracting logs from haskell-server..."
+  SINK_POD=$(kubectl get pods --selector=name=haskell-server -o jsonpath='{.items[*].metadata.name}')
+  kubectl cp "${SINK_POD}:/opt/server/sw-log.txt" "${RESULTS_DIR}/serial-log.txt"
+}
+
+benchmark() {
+  log "Starting benchmark..."
+  [[ -d "${RESULTS_DIR}" ]] || mkdir "${RESULTS_DIR}"
+  build $1
+  start
+  log "All pods running, waiting $(($TEST_TIME/60))m..."
+  sleep $TEST_TIME
+  extract
 }
 
 install() {
   build $1
   start
-  attach
+  output
 }
+
 
 stop() {
   log "Stopping pipeline..."
-  kubectl delete services,deployments -l name=haskell-server --now
+  # kubectl delete services,deployments -l name=haskell-server --now
   declare -a names=()
-  for dir in "${dirs[@]:1}"
+  for dir in "${dirs[@]}"
   do
     n=haskell-$dir
     names+=($n)
@@ -80,7 +116,7 @@ stop() {
 
 
 clean() {
-  eval $(minikube docker-env)
+  # eval $(minikube docker-env)
   log "Removing Kubernetes assets..."
   kubectl delete deployments,pods,services -l all-flag=striot --now
   log "Removing docker containers..."
@@ -104,9 +140,6 @@ clean() {
 
 
 
-
-
-
 help() {
   echo "-----------------------------------------------------------------------"
   echo "                      Available commands                              -"
@@ -115,7 +148,7 @@ help() {
   echo "   > install - Build, Start and Attach"
   echo "   > build - To build the Docker images"
   echo "   > start - To start the pipeline containers"
-  echo "   > attach - Connect to server to see output"
+  echo "   > output - Follow log output from server"
   echo "   > stop - To stop the pipeline containers"
   echo "   > clean - Remove the pipeline containers and all assets"
   echo "   > help - Display this help"
@@ -125,12 +158,12 @@ help() {
 
 
 log() {
-  echo "$BLUE > $1 $NORMAL"
+  echo "$BLUE > $1 $NORMAL" | ts '[%d-%m-%Y %H:%M:%.S]'
 }
 
 error() {
   echo ""
-  echo "$RED >>> ERROR - $1$NORMAL"
+  echo "$RED >>> ERROR - $1$NORMAL" | ts '[%d-%m-%Y %H:%M:%.S]'
 }
 
 
--- examples/k8s-pipeline/server/Server.hs
+++ examples/k8s-pipeline/server/Server.hs
@@ -1,20 +1,85 @@
+{-# LANGUAGE FlexibleContexts #-}
 import System.IO
+import System.IO.Unsafe (unsafeInterleaveIO)
 import Data.List
+
 import Striot.FunctionalProcessing
 import Striot.FunctionalIoTtypes
 import Striot.Nodes
-import Network.Socket
+import Network.Socket (ServiceName)
+
+import Data.Time (getCurrentTime)
+import Data.Time.Clock (UTCTime)
+import qualified Data.ByteString.Lazy.Char8 as BLC (hPutStrLn)
+import Data.Aeson
+import qualified Data.Text as T
+import Control.Monad (when)
+import Control.Concurrent
+import Control.Concurrent.STM
 
 listenPort = "9001" :: ServiceName
+fileName = "sw-log.txt"
+
 
 main :: IO ()
-main = nodeSink streamGraphid printStream listenPort
+main = do
+    chan <- newTChanIO
+    writeFile fileName ""
+    hdl <- openFile fileName WriteMode
+    hSetBuffering hdl NoBuffering
+    _ <- forkFinally (writeToFile hdl chan) (\_ -> hClose hdl)
+    nodeSink streamGraphid (sinkToChan chan) listenPort
+    -- nodeSink streamGraphid printStream listenPort
 
-streamGraph1 :: Stream String -> Stream [String]
-streamGraph1 s = streamWindow (chop 2) $ streamMap (\st-> "Incoming Message at Server: " ++ st) s
 
-streamGraphid :: Stream String -> Stream String
-streamGraphid = streamMap Prelude.id
+streamGraphid :: Stream T.Text -> Stream T.Text
+streamGraphid = streamMap (\x -> Prelude.id x)
 
-printStream:: Show alpha => Stream alpha -> IO ()
-printStream = mapM_ print
+
+sinkToChan :: (ToJSON alpha) => TChan (Event (alpha, UTCTime)) -> Stream alpha -> IO ()
+sinkToChan chan stream = do
+    out <- mapTime stream
+    mapM_ (writeEvent chan) out
+  where
+      writeEvent chan x = atomically $ writeTChan chan x
+
+
+writeToFile :: (ToJSON alpha) => Handle -> TChan (Event (alpha, UTCTime)) -> IO ()
+writeToFile hdl chan = do
+    stream <- readEventsTChan chan
+    hPutLines'' hdl stream
+
+
+readEventsTChan :: ToJSON alpha => TChan (Event (alpha, UTCTime)) -> IO (Stream (alpha, UTCTime))
+readEventsTChan eventChan = System.IO.Unsafe.unsafeInterleaveIO $ do
+    x <- atomically $ readTChan eventChan
+    xs <- readEventsTChan eventChan
+    return (x : xs)
+
+
+-- printStreamDelay :: (ToJSON alpha, Show alpha) => Stream alpha -> IO ()
+-- printStreamDelay stream = do
+--     hdl <- openFile fileName WriteMode
+--     hSetBuffering hdl NoBuffering
+--     output <- mapTime stream
+--     hPutLines'' hdl output
+
+
+mapTime :: Stream alpha -> IO (Stream (alpha, UTCTime))
+mapTime (e@(E id t v):r) = unsafeInterleaveIO $ do
+    x <- msg e v
+    xs <- mapTime r
+    return (x:xs)
+  where
+    msg x v = do
+        now <- getCurrentTime
+        return x { value = (v, now) }
+
+
+hPutLines'' :: ToJSON (Event alpha) => Handle -> Stream alpha -> IO ()
+hPutLines'' handle (x:xs) = do
+    writable <- hIsWritable handle
+    when writable $ do
+        BLC.hPutStrLn handle (encode x)
+        hFlush handle
+        hPutLines'' handle xs
