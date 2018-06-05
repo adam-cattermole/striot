fe7c8619c9d5efd5a431502ae9a59ff5b59e80b1
Adam Cattermole <a.d.cattermole@gmail.com>
2018-06-05 17:08:42 +0100

    Changes to collect performance results

--- examples/k8s-pipeline/client2/Client2.hs
+++ examples/k8s-pipeline/client2/Client2.hs
@@ -5,6 +5,7 @@ import Striot.FunctionalProcessing
 import Striot.Nodes
 import System.Environment
 import System.IO
+import Control.DeepSeq
 
 brokerPort =  "61613" :: ServiceName
 connectPort = "9001" :: ServiceName
@@ -14,7 +15,17 @@ main = do
     podName <- getEnv "HOSTNAME"
     brokerHost <- getEnv "AMQ_STOMP_SERVICE_HOST"
     connectHost <- getEnv "HASKELL_SERVER_SERVICE_HOST"
-    nodeLinkAmq (streamGraph1 podName) brokerHost brokerPort connectHost connectPort
+    nodeLinkAmq streamGraphid brokerHost brokerPort connectHost connectPort
 
 streamGraph1 :: String -> Stream String -> Stream String
 streamGraph1 podName = streamMap (\st-> podName ++ ": " ++ st)
+
+streamGraphid :: Stream String -> Stream String
+streamGraphid = Prelude.id
+
+streamGraphLoad :: Stream String -> Stream String
+streamGraphLoad = streamMap (\x -> factorial 213000 `deepseq` x)
+
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
